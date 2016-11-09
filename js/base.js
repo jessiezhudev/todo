5abed3e8196cfd078ed3262198e41261a43434d9
jessie.zhu <zhuyudian@zhuyudiandeMacBook-Pro.local>
2016-11-09 10:18:47 +0800

    change according to the course

--- js/base.js
+++ js/base.js
@@ -1,6 +1,9 @@
 ;(function(){
+  'use strict';
     $("#submit").click(function(){
         var input_content = $('#input-content').val();
-        $('.todo-result').append("<div class='todo-item'><div class='todo-list'><input type='radio'/>input_content</div><button class='btn-list'>+</button><button class='btn-list'>x</button></div>");
+        var html = '';
+        html = '<div class="todo-item">'+'<div class="todo-list">'+'<input type="radio"/>' + input_content +  '</div>' + '<button class="btn-list">'+"+"+'</button>'+'<button class="btn-list">'+"x"+'</button>'+'</div>';
+        $('.todo-result').append(html);
     });
 })();
